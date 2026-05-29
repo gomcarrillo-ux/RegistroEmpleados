d0d697a36b0e2b1bd181a0820d524dee876bcac2
gabrielito <gomcarrillo@uamv.edu.ni>
2026-05-29 12:35:20 -0600

    Agregue la funcion mmenu y main

--- RegistroEmpleados/Program.cs
+++ RegistroEmpleados/Program.cs
@@ -1,5 +1,5 @@
 ﻿Empleado[] empleados = new Empleado[10];
-void agregarEmpleados(int pos)
+void agregarEmpleado(int pos)
 {
     Console.Write("Nombres: ");
     empleados[pos].nombres = Console.ReadLine()!;
@@ -13,6 +13,32 @@ void agregarEmpleados(int pos)
     Console.WriteLine("Registro guardado exitosamente");
     Console.ResetColor();
 }
+    int menu()
+    {
+        Console.Write("1. Agregar \n2. Mostrar \n3. Eliminar \n4. Salir \nDigite su opción: ");
+        int op = int.Parse(Console.ReadLine()!);
+        return op;
+    }
+
+    int main()
+    {
+        int op = 0, i = 0;
+
+    do
+    {
+        Console.WriteLine($"Registro # {i + 1}");
+        op = menu();
+        switch (op)
+        {
+            case 1:
+                agregarEmpleado(i++);
+                break;
+        }
+        } while (op != 4);
+        
+       return 0;
+    }
+main();
 struct Empleado
 {
     public string nombres;
