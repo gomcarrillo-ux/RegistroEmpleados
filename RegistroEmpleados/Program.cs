9b3f41206c3e6f17c762b8499eef26fab3b39177
gabrielito <gomcarrillo@uamv.edu.ni>
2026-06-08 11:57:26 -0600

    update

--- RegistroEmpleados/Program.cs
+++ RegistroEmpleados/Program.cs
@@ -1,16 +1,20 @@
-﻿Empleado[] empleados = new Empleado[10];
+﻿
+using System.Runtime.Serialization.Formatters;
+
+Empleado[] empleados = new Empleado[10];
+
 void agregarEmpleado(int pos)
 {
     Console.Write("Nombres: ");
-    empleados[pos].nombres = Console.ReadLine()!;
+    empleados[pos].nombres = Console.ReadLine();
     Console.Write("Apellidos: ");
-    empleados[pos].apellidos = Console.ReadLine()!;
+    empleados[pos].apellidos = Console.ReadLine();
     Console.Write("Cargo: ");
-    empleados[pos].cargo = Console.ReadLine()!;
+    empleados[pos].cargo = Console.ReadLine();
     Console.Write("Salario: ");
-    empleados[pos].salario = double.Parse(Console.ReadLine()!);
+    empleados[pos].salario = Double.Parse(Console.ReadLine());
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Registro guardado exitosamente");
+    Console.WriteLine("Registro guardado satisfactoriamente");
     Console.ReadKey();
     Console.ResetColor();
 }
@@ -22,40 +26,44 @@ void mostrarDatos(int pos)
     {
         Console.WriteLine($"Nombre: {empleados[i].nombres} \nApellidos: {empleados[i].apellidos} \nCargo: {empleados[i].cargo} \nSalario: {empleados[i].salario}");
     }
-    Console.WriteLine("Registros mostrados exitosamente");
     Console.ReadKey();
 }
 
 int menu()
 {
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("1. Agregar \n2. Mostrar \n3. Guardar \n4. Salir \nDigite su opción: ");
+    Console.Clear();
+    Console.Write("1. Agregar \n2. Mostrar \n3. Guardar \n4.Salir \nDigite su opcion: ");
     Console.ForegroundColor = ConsoleColor.Blue;
-    int op = int.Parse(Console.ReadLine()!);
+    int op = int.Parse(Console.ReadLine());
     Console.ResetColor();
     return op;
 }
 
 void guardarEmpleados(int pos)
-{ 
-StreamWriter archivo = new StreamWriter("C:\\semana12\\Empleados.csv");
+{
+    StreamWriter archivo = new StreamWriter("C:\\Programacion\\empleados.csv");
     for (int i = 0; i < pos; i++)
     {
-        archivo.WriteLine($"{empleados[i].nombres};{empleados[i].apellidos};{empleados[i].cargo};{empleados[i].salario}");
+        archivo.WriteLine($"{empleados[i].nombres}; {empleados[i].apellidos}; {empleados[i].cargo}; {empleados[i].salario}");
     }
     archivo.Close();
     Console.WriteLine("Registros guardados.");
     Console.ReadKey();
 }
-    int main()
-    {
-        int op = 0, i = 0;
+
+int main()
+{
+    int op = 0, i = 0;
+
     Console.Write("Cargando");
-    for(int j = 0; j < 10; j++)
+    for (int j = 0; j < 10; j++)
     {
         Thread.Sleep(1000);
         Console.Write(".");
+
     }
+    Console.WriteLine("\n He terminado de cargar.");
 
     do
     {
@@ -73,14 +81,18 @@ StreamWriter archivo = new StreamWriter("C:\\semana12\\Empleados.csv");
                 guardarEmpleados(i);
                 break;
             default:
-                Console.WriteLine("Opción invalida...");
+                Console.WriteLine("Opción invalida....");
                 break;
+
         }
-        } while (op != 4);
-        
-       return 0;
-    }
+    } while (op != 4);
+
+    return 0;
+}
+
 main();
+
+
 struct Empleado
 {
     public string nombres;
